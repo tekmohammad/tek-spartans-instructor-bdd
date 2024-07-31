9ec61a88c0b58cf18d6644126334ba1f2d80773d
Yousuf897 <ahmad.samandar@tekschool.us>
2024-07-30 20:28:28 -0400

    make code maintainable

---
+++ src/test/java/tek/bdd/steps/SetupTestSteps.java
@@ -0,0 +1,26 @@
+package tek.bdd.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import tek.bdd.utility.SeleniumUtility;
+
+public class SetupTestSteps extends SeleniumUtility {
+
+    @Given("Open browser and navigate to retail app")
+    public void openBrowserAndNavigate() {
+        setupBrowser();
+    }
+
+    @Then("validate top left corner is TEKSCHOOL")
+    public void validateTopLeftLogo() {
+       String text = getElementText(By.className("top-nav__logo"));
+
+        System.out.println(text);
+    }
+
+    @Then("Close the browser")
+    public void closeBrowser() {
+        quitBrowser();
+    }
+}
---
+++ src/test/resources/features/SetupTest.feature
@@ -0,0 +1,6 @@
+Feature: This feature to setup framework
+
+  Scenario: Validate top left corner logo
+    Given Open browser and navigate to retail app
+    Then validate top left corner is TEKSCHOOL
+    Then Close the browser

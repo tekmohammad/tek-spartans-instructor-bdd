89f66b508edef625d65e5ab7e8c5571f56cd2404
Mohammad Shokriyan <mohammad@tekschool.us>
2024-08-07 19:53:23 -0400

    implement reading properties file

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -3,16 +3,39 @@ package tek.bdd.base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public abstract class BaseSetup {
 
     //Encapsulating driver instance
     private static WebDriver driver;
+    private Properties properties;
+
+    //Find the full path to file
+    //FileInputStream to read the file
+    //Properties and load the FileInputStream to the properties
+    public BaseSetup() {
+        //System.getProperty("user.dir") return the locator of your project.
+        try {
+            String configFilePath = System.getProperty("user.dir") +
+                    "/src/test/resources/configs/dev-config.properties";
+            File file = new File(configFilePath);
+            FileInputStream fileInputStream = new FileInputStream(file);
+            properties = new Properties();
+            properties.load(fileInputStream);
+        }catch (IOException ex) {
+            throw new RuntimeException("Something wrong with Config file", ex);
+        }
+    }
 
     public void setupBrowser() {
         driver = new ChromeDriver();
-        driver.get("https://retail.tekschool-students.com/");
+       String url = properties.getProperty("ui.url");
+        driver.get(url);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }

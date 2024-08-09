68f5b43f42d6570100617fc123ea306fe69daae9
Mohammad Shokriyan <mohammad@tekschool.us>
2024-08-08 21:45:56 -0400

    Using interface in framework

--- src/test/java/tek/bdd/base/BaseSetup.java
+++ src/test/java/tek/bdd/base/BaseSetup.java
@@ -9,7 +9,12 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import tek.bdd.browsers.BaseBrowser;
+import tek.bdd.browsers.ChromeBrowser;
+import tek.bdd.browsers.EdgeBrowser;
+import tek.bdd.browsers.FireFoxBrowser;
 
+import javax.swing.plaf.metal.MetalLookAndFeel;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -46,24 +51,18 @@ public abstract class BaseSetup {
         String browserType = properties.getProperty("ui.browser");
         boolean isHeadless = Boolean.parseBoolean(properties.getProperty("ui.browser.headless"));
         LOGGER.info("Running on browser {} and isHeadless {}", browserType, isHeadless);
-        if (browserType.equalsIgnoreCase("chrome")) {
-            ChromeOptions options = new ChromeOptions();
-            if (isHeadless)
-                options.addArguments("--headless");
-            driver = new ChromeDriver(options);
-        } else if (browserType.equalsIgnoreCase("edge")) {
-            EdgeOptions options = new EdgeOptions();
-            if (isHeadless) options.addArguments("--headless");
-            driver = new EdgeDriver(options);
-        } else if (browserType.equalsIgnoreCase("firefox")) {
-            FirefoxOptions options = new FirefoxOptions();
-            if (isHeadless)
-                options.addArguments("--headless");
-            driver = new FirefoxDriver(options);
-        } else {
-            throw new RuntimeException("Wrong browser type choose between chrome, firefox or edge");
-        }
+        BaseBrowser browser;
 
+        if(browserType.equalsIgnoreCase("chrome"))
+            browser = new ChromeBrowser();
+        else if (browserType.equalsIgnoreCase("edge"))
+            browser = new EdgeBrowser();
+        else if (browserType.equalsIgnoreCase("firefox"))
+            browser = new FireFoxBrowser();
+        else
+            throw new RuntimeException("Wrong browser type choose between chrome, firefox or edge");
+
+       driver = browser.openBrowser(isHeadless);
 
         String url = properties.getProperty("ui.url");
         LOGGER.debug("Using URL {}", url);
---
+++ src/test/java/tek/bdd/browsers/BaseBrowser.java
@@ -0,0 +1,8 @@
+package tek.bdd.browsers;
+
+import org.openqa.selenium.WebDriver;
+
+public interface BaseBrowser {
+
+    WebDriver openBrowser(boolean isHeadless);
+}
